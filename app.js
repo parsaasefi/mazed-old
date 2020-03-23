24e005357abe3f7dfd866bfa8d1b648388b3deff
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-23 09:49:54 -0700

    added essential packages

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.length('/', (req, res) => {
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
   res.send('Hello from Mazed');
 });
 
