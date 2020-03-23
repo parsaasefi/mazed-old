2d9ccc6a7d40481f4ad3f4570d4e00beec0c414a
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-23 10:22:17 -0700

    added event emitter

--- controllers/process.js
+++ controllers/process.js
@@ -1,14 +1,25 @@
 const { validationResult } = require('express-validator');
+const events = require('events');
 
 class ProcessController {
   static getProcessor(req, res) {
+    const eventEmitter = new events.EventEmitter();
+
+    eventEmitter.on('info', (info) => {
+      res.send('Cool');
+    });
+
+    eventEmitter.on('error', (err) => {
+      res.json({ errors: err });
+    });
+
     const validatorErrors = validationResult(req);
 
-    if (!validatorErrors.errors()) {
-      return res.json({ errors: validatorErrors.array() });
+    if (!validatorErrors.isEmpty()) {
+      return eventEmitter.emit('error', validatorErrors.array());
     }
 
-    return true;
+    return res.send('Cool!');
   }
 }
 
