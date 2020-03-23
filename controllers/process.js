7fd194d55e87d33286a028ac6c5c729c0f6ca25b
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-23 11:35:52 -0700

    added uri follower to the processor

--- controllers/process.js
+++ controllers/process.js
@@ -4,15 +4,19 @@ const events = require('events');
 const URIHelper = require('../helpers/uri');
 
 class ProcessController {
-  static getProcessor(req, res) {
+  static async getProcessor(req, res) {
     const eventEmitter = new events.EventEmitter();
 
-    // Listener to handle the given information
+    /*
+     * Listener to handle the given information
+     */
     eventEmitter.on('info', info => {
-      res.send(info);
+      res.json(info);
     });
 
-    // Listener to handle the errors
+    /*
+     * Listener to handle the errors
+     */
     eventEmitter.on('error', err => {
       res.json({ errors: err });
     });
@@ -31,7 +35,23 @@ class ProcessController {
       return eventEmitter.emit('error', 'IS_NOT_SHORTENED');
     }
 
-    return res.send(uri);
+    /*
+     * Don't use Redis cache if forceUpdate is set to true
+     */
+    if (forceUpdate) {
+      const destination = await URIHelper.follow(uri);
+      const safe = true;
+      const lastUpdate = Date.now();
+      const info = {
+        destination,
+        safe,
+        lastUpdate,
+      };
+
+      eventEmitter.emit('info', info);
+    }
+
+    return true;
   }
 }
 
