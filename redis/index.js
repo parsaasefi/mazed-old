8c9182e6ef38e1f79755cdbc70dcdf66c630d5e0
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-24 11:01:12 -0700

    added get and add methods for Redis

--- redis/index.js
+++ redis/index.js
@@ -1,6 +1,7 @@
 const redis = require('redis');
 const dotenv = require('dotenv');
 const path = require('path');
+const url = require('url');
 
 const config = require('./config.json');
 
@@ -17,6 +18,36 @@ class Redis {
     if (config[env].password) options.password = config[env].password;
 
     this.client = redis.createClient(options);
+    this.expireTime = 10; // Seconds
+  }
+
+  addURI(uri, info) {
+    return new Promise((resolve, reject) => {
+      const { hostname, pathname } = url.parse(uri);
+
+      this.client.setex(
+        hostname + pathname,
+        this.expireTime,
+        JSON.stringify(info),
+        (err, res) => {
+          if (err) return reject(err);
+
+          return resolve(res);
+        }
+      );
+    });
+  }
+
+  getURI(uri) {
+    return new Promise((resolve, reject) => {
+      const { hostname, pathname } = url.parse(uri);
+
+      this.client.get(hostname + pathname, (err, res) => {
+        if (err) return reject(err);
+
+        return resolve(JSON.parse(res));
+      });
+    });
   }
 }
 
