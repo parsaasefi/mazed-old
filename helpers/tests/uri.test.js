3ad61fad7cb9f7a08b7c7ab4f8ab30d9cb8185c3
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-24 11:36:20 -0700

    added blacklist.txt

---
+++ helpers/tests/uri.test.js
@@ -0,0 +1,26 @@
+const URIHelper = require('../uri');
+
+test('Add protocol to the URI', () => {
+  expect(URIHelper.addProtocol('http://sample.com')).toBe('http://sample.com');
+  expect(URIHelper.addProtocol('sample.com')).toBe('https://sample.com');
+  expect(URIHelper.addProtocol('hTtP://SamplE.com')).toBe('hTtP://SamplE.com');
+  expect(URIHelper.addProtocol('sample.com/TeSt')).toBe(
+    'https://sample.com/TeSt'
+  );
+});
+
+test('Remove www from the URI', () => {
+  expect(URIHelper.removeWWW('https://www.sample.com')).toBe(
+    'https://sample.com/'
+  );
+  expect(URIHelper.removeWWW('https://WwW.sample.com')).toBe(
+    'https://sample.com/'
+  );
+  expect(URIHelper.removeWWW('https://sample.com')).toBe('https://sample.com/');
+});
+
+test('Check if the URI is shortened', () => {
+  expect(URIHelper.isShortened('https://bit.ly')).toBe(true);
+  expect(URIHelper.isShortened('https://BiT.ly')).toBe(true);
+  expect(URIHelper.isShortened('https://sample.com')).toBe(false);
+});
