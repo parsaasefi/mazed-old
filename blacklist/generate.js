d8cbac505659774be8d8ea5dc797569fd017d650
Parsa Asefi <parsaasefi@protonmail.com>
2020-03-25 10:38:11 -0700

    tried to import sql

--- blacklist/generate.js
+++ blacklist/generate.js
@@ -1,35 +1,60 @@
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
-const Datastore = require('nedb');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
+const config = require('../config/config.json');
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+});
+
+const table = 'blacklist';
 const input = path.join(__dirname, 'dataset/blacklist.txt');
-const database = path.join(__dirname, 'database/blacklist.db');
-const hosts = [];
-
-const lineReader = readline.createInterface({
-  input: fs.createReadStream(input),
-  crlfDelay: Infinity,
+const env = process.env.NODE_ENV || 'development';
+const { host, port, username: user, password, database } = config[env];
+const connection = mysql.createConnection({
+  host,
+  port,
+  user,
+  password,
+  database,
 });
 
-lineReader.on('line', line => {
-  const data = {
-    host: line.slice(1),
-  };
-  hosts.push(data);
-});
+connection.connect(connectionError => {
+  if (connectionError) {
+    throw new Error(connectionError);
+  }
 
-lineReader.on('close', () => {
-  const blacklist = new Datastore({
-    filename: database,
-    autoload: true,
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(input),
+    crlfDelay: Infinity,
   });
 
-  blacklist.insert(hosts, err => {
-    if (err) {
-      throw new Error(err);
-    }
+  const linesPerQuery = 100;
+  let query = '';
+  let counter = 1;
 
-    console.log('Blacklist database has been generated successfully.');
+  query += `DROP TABLE IF EXISTS ${table};`;
+  query += `CREATE TABLE ${table} (id INT AUTO_INCREMENT PRIMARY KEY, host VARCHAR(255));`;
+
+  lineReader.on('line', line => {
+    const newQuery = `INSERT INTO ${table} (host) VALUES ('${line.slice(1)}');`;
+
+    if (query.split(';').length < linesPerQuery) {
+      query += newQuery;
+    } else {
+      const queryToImport = query;
+      query = newQuery;
+
+      connection.query(queryToImport, queryError => {
+        if (queryError) {
+          throw new Error(queryError);
+        }
+
+        console.log(`Added pack number ${counter++}`);
+      });
+    }
   });
 });
